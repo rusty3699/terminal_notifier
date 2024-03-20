4adb1f17e3cb0faa48262e1799cd20dfa9f9e9fc
Anish Tipnis <rusty3699@gmail.com>
2024-03-20 19:17:08 +0530

    need to fix sound

---
+++ build/lib/terminal_notifier/notifier.py
@@ -0,0 +1,32 @@
+from plyer import notification
+from playsound import playsound
+
+def notify_execution(scenario="success", custom_message=None):
+    try:
+        # Dictionary to map default scenarios to notification messages
+        scenario_messages = {
+            "success": "Command executed successfully!",
+            "failure": "Command terminated with an error!",
+            "user_input_required": "Command is waiting for user input!",
+            "crash": "Command crashed!",
+            "unknown": "Unknown scenario!"
+        }
+
+        # Use custom message if specified
+        if scenario == "custom" and custom_message:
+            notification_message = custom_message
+        else:
+            notification_message = scenario_messages.get(scenario, scenario_messages["unknown"])
+
+        # Display notification with a default timeout of 5 seconds
+        notification.notify(
+            title="Command Execution",
+            message=notification_message,
+            timeout=5
+        )
+        
+        playsound("terminal_notifier/sound.mp3")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="terminal-notifier",  # Replace with your package name
-    version="0.1.0",  # Replace with your package version
+    name="terminal_notifier",  # Replace with your package name
+    version="0.1",  # Replace with your package version
     author="Anish Tipnis",
-    author_email="rusty3699@gmail.com",
-    description="A package for notifying users about command execution in the terminal",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rusty3699/terminal-notifier",  # Replace with your GitHub repository URL
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3"
-    ],
     python_requires=">=3.6",
     install_requires=[
         "plyer",
